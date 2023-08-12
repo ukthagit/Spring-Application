2dd9983ac82f3d3f14190c6a0026277123348b39
ukthagit <git.uktha@gmail.com>
2023-08-12 14:17:25 -0400

    committing all files

---
+++ src/main/java/Spring_Boot_Application/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package Spring_Boot_Application.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spring_Boot_Application/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring_Boot_Application.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
